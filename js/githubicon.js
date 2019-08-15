e6920151ee864c5ba9ae8a4e67a3b89d12d790dd
astronomizedev <51801389+astronomizedev@users.noreply.github.com>
2019-08-14 19:13:29 -0600

    Add files via upload

---
+++ js/githubicon.js
@@ -0,0 +1,9 @@
+function hoverOver() {
+  var img = document.getElementById('github')
+  img.src = "images/github-icon-white.png"
+}
+
+function notHovering() {
+  var img = document.getElementById('github')
+  img.src = "images/github-icon.png"
+}
